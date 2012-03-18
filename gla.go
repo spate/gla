2631b48f2b6de68127a1f2987608b318561c5ab1
James Helferty <windmist@gmail.com>
2012-03-17 21:44:28 -0400

    Add CompressedTexImage2DFromImage, Fix the others
    - Added CompressedTexImage2DFromImage for loading compressed textures
      from the github.com/spate/glimage Dxt1,Dxt3,Dxt5 containers
    - Tested and fixed the basic image loading routine to not crash. Heh.

--- gla.go
+++ gla.go
@@ -7,13 +7,15 @@
 // to accomplish this, gla may at times clobber additional pieces of
 // OpenGL state beyond just the one implied by the function name.
 // Each function documents which GL state is modified in this manner.
-
+//
 // It is intended that this package integrate as seemlessly as possible
 // with existing OpenGL bindings such as github.com/banthar/gl
-
+//
 // Naming of functions contained herein attempts to mimic the verbose
 // nature of the D3DX functions while still suggesting the underlying
 // OpenGL calls used.
+//
+// (P.S. - If something isn't working, check if you forgot to call gl.Init)
 
 package gla
 
@@ -35,9 +37,11 @@ package gla
 import "C"
 import "unsafe"
 import "reflect"
+import "fmt"
 import "image"
 import "image/draw"
 import "github.com/banthar/gl"
+import "github.com/spate/glimage"
 
 type GLenum gl.GLenum
 type GLbitfield gl.GLbitfield
@@ -59,12 +63,23 @@ type GLfloat gl.GLfloat
 type GLdouble gl.GLdouble
 
 type imageInfo struct {
-	Data       []uint8
-	RowLength  int
-	Format     GLenum
-	Type       GLenum
-	Compressed bool
+	Data      []uint8
+	RowLength int
+	Format    GLenum
+	Type      GLenum
 }
+
+// The following are missing from github.com/banthar/gl
+const (
+	COMPRESSED_RGB_S3TC_DXT1        = 0x83F0
+	COMPRESSED_RGBA_S3TC_DXT1       = 0x83F1
+	COMPRESSED_RGBA_S3TC_DXT3       = 0x83F2
+	COMPRESSED_RGBA_S3TC_DXT5       = 0x83F3
+	COMPRESSED_SRGB_S3TC_DXT1       = 0x8C4C
+	COMPRESSED_SRGB_ALPHA_S3TC_DXT1 = 0x8C4D
+	COMPRESSED_SRGB_ALPHA_S3TC_DXT3 = 0x8C4E
+	COMPRESSED_SRGB_ALPHA_S3TC_DXT5 = 0x8C4F
+)
 
 // Returns GL parameters for loading data from the subrect "r" of image "img"
 func getImageInfo(i image.Image) imageInfo {
@@ -77,34 +92,34 @@ func getImageInfo(i image.Image) imageInfo {
 	case *image.Alpha:
 		img, _ := i.(*image.Alpha)
 		data, stride, bpp = img.Pix, img.Stride, 1
-		info.Format, info.Type, info.Compressed = gl.ALPHA, gl.UNSIGNED_BYTE, false
+		info.Format, info.Type = gl.ALPHA, gl.UNSIGNED_BYTE
 	case *image.Alpha16:
 		img, _ := i.(*image.Alpha16)
 		data, stride, bpp = img.Pix, img.Stride, 2
-		info.Format, info.Type, info.Compressed = gl.ALPHA, gl.UNSIGNED_SHORT, false
+		info.Format, info.Type = gl.ALPHA, gl.UNSIGNED_SHORT
 	case *image.Gray:
 		img, _ := i.(*image.Gray)
 		data, stride, bpp = img.Pix, img.Stride, 1
-		info.Format, info.Type, info.Compressed = gl.LUMINANCE, gl.UNSIGNED_BYTE, false
+		info.Format, info.Type = gl.LUMINANCE, gl.UNSIGNED_BYTE
 	case *image.Gray16:
 		img, _ := i.(*image.Gray16)
 		data, stride, bpp = img.Pix, img.Stride, 2
-		info.Format, info.Type, info.Compressed = gl.LUMINANCE, gl.UNSIGNED_SHORT, false
+		info.Format, info.Type = gl.LUMINANCE, gl.UNSIGNED_SHORT
 	case *image.RGBA:
 		img, _ := i.(*image.RGBA)
 		data, stride, bpp = img.Pix, img.Stride, 4
-		info.Format, info.Type, info.Compressed = gl.RGBA, gl.UNSIGNED_BYTE, false
+		info.Format, info.Type = gl.RGBA, gl.UNSIGNED_BYTE
 	case *image.RGBA64:
 		img, _ := i.(*image.RGBA64)
 		data, stride, bpp = img.Pix, img.Stride, 8
-		info.Format, info.Type, info.Compressed = gl.RGBA, gl.UNSIGNED_SHORT, false
+		info.Format, info.Type = gl.RGBA, gl.UNSIGNED_SHORT
 	default:
 		// for unknown types, convert to RGBA8
 		r := i.Bounds()
 		img := image.NewRGBA(r)
 		draw.Draw(img, r.Sub(r.Min), i, r.Min, draw.Src)
 		info.Format, info.Type = gl.RGBA, gl.UNSIGNED_BYTE
-		info.Data, info.RowLength, info.Compressed = img.Pix, img.Stride/4, false
+		info.Data, info.RowLength = img.Pix, img.Stride/4
 		return info
 	}
 
@@ -132,18 +147,14 @@ func TexImage2DFromImage(target GLenum, level int, internalformat int, border in
 	}
 
 	info := getImageInfo(img)
+	ptr := unsafe.Pointer(reflect.ValueOf(info.Data).Index(0).UnsafeAddr())
 
-	if info.Compressed {
-		// unhandled by this function
-		return
-	} else {
-		C.glPixelStorei(C.GLenum(gl.UNPACK_ALIGNMENT), C.GLint(1))
-		C.glPixelStorei(C.GLenum(gl.UNPACK_ROW_LENGTH), C.GLint(info.RowLength))
-		C.glTexImage2D(C.GLenum(target), C.GLint(level), C.GLint(internalformat),
-			C.GLsizei(bounds.Dx()), C.GLsizei(bounds.Dy()), C.GLint(border),
-			C.GLenum(info.Format), C.GLenum(info.Type),
-			unsafe.Pointer(reflect.ValueOf(info.Data).UnsafeAddr()))
-	}
+	C.glPixelStorei(C.GLenum(gl.UNPACK_ALIGNMENT), C.GLint(1))
+	C.glPixelStorei(C.GLenum(gl.UNPACK_ROW_LENGTH), C.GLint(info.RowLength))
+	C.glTexImage2D(C.GLenum(target), C.GLint(level), C.GLint(internalformat),
+		C.GLsizei(bounds.Dx()), C.GLsizei(bounds.Dy()), C.GLint(border),
+		C.GLenum(info.Format), C.GLenum(info.Type),
+		ptr)
 }
 
 // TexSubImage2DFromImage loads texture data from an image.Image into the currently
@@ -160,17 +171,87 @@ func TexSubImage2DFromImage(target GLenum, level int, dest image.Rectangle, img 
 	}
 
 	info := getImageInfo(img)
+	ptr := unsafe.Pointer(reflect.ValueOf(info.Data).Index(0).UnsafeAddr())
 
-	if info.Compressed {
-		// unhandled by this function
-		return
-	} else {
-		C.glPixelStorei(C.GLenum(gl.UNPACK_ALIGNMENT), C.GLint(1))
-		C.glPixelStorei(C.GLenum(gl.UNPACK_ROW_LENGTH), C.GLint(info.RowLength))
-		C.glTexSubImage2D(C.GLenum(target), C.GLint(level),
-			C.GLint(dest.Min.X), C.GLint(dest.Min.Y),
-			C.GLsizei(dest.Dx()), C.GLsizei(dest.Dy()),
-			C.GLenum(info.Format), C.GLenum(info.Type),
-			unsafe.Pointer(reflect.ValueOf(info.Data).UnsafeAddr()))
+	C.glPixelStorei(C.GLenum(gl.UNPACK_ALIGNMENT), C.GLint(1))
+	C.glPixelStorei(C.GLenum(gl.UNPACK_ROW_LENGTH), C.GLint(info.RowLength))
+	C.glTexSubImage2D(C.GLenum(target), C.GLint(level),
+		C.GLint(dest.Min.X), C.GLint(dest.Min.Y),
+		C.GLsizei(dest.Dx()), C.GLsizei(dest.Dy()),
+		C.GLenum(info.Format), C.GLenum(info.Type),
+		ptr)
+}
+
+// Returns GL parameters for loading data from the subrect "r" of image "img"
+func getCompressedImageInfo(i image.Image) (imageInfo, error) {
+	var data []uint8
+	var stride int
+	var blocksize int // overall number of bytes in a block
+	var blockdim int  // size of one dimension; assume square blocks
+	var info imageInfo
+
+	switch i.(type) {
+	case *glimage.Dxt1:
+		img, _ := i.(*glimage.Dxt1)
+		data, stride, blockdim, blocksize = img.Pix, img.Stride, 4, 8
+		info.Format = COMPRESSED_RGBA_S3TC_DXT1_EXT
+	case *glimage.Dxt3:
+		img, _ := i.(*glimage.Dxt3)
+		data, stride, blockdim, blocksize = img.Pix, img.Stride, 4, 16
+		info.Format = COMPRESSED_RGBA_S3TC_DXT3_EXT
+	case *glimage.Dxt5:
+		img, _ := i.(*glimage.Dxt5)
+		data, stride, blockdim, blocksize = img.Pix, img.Stride, 4, 16
+		info.Format = COMPRESSED_RGBA_S3TC_DXT5_EXT
+	default:
+		return imageInfo{}, fmt.Errorf("gla: unrecognized texture format")
 	}
+
+	bounds := i.Bounds()
+	w, h := bounds.Dx(), bounds.Dy()
+	sub_stride := (w + (blockdim - 1)) / blockdim * blocksize // stride of just the data we want
+	if sub_stride != stride {
+		//fmt.Printf("stride mismatch: %v != %v\n", sub_stride, stride)
+		data_xoff := bounds.Min.X + (blockdim-1)/blockdim*blocksize
+		data_yoff := bounds.Min.Y + (blockdim-1)/blockdim*stride
+		data_offset := data_yoff + data_xoff
+
+		// need to allocate a new chunk of memory and copy the data in
+		// because compressed loads don't have pixelstore state :(
+		pix := make([]uint8, ((w+(blockdim-1))/blockdim)*((h+(blockdim-1))/blockdim)*blocksize)
+
+		var c int
+		for i := 0; i < h/blockdim; i++ {
+			c = copy(pix[i*sub_stride:(i+1)*sub_stride], data[data_offset+i*stride:data_offset+(i+1)*stride])
+			if c != sub_stride {
+				return imageInfo{}, fmt.Errorf("gla: cannot copy subimage")
+			}
+		}
+		data = pix
+	}
+	info.Data = data
+
+	return info, nil
+}
+
+// CompressedTexImage2DFromImage loads texture data from an image.Image into the
+// currently bound GL texture using the glCompressedTexImage2D call.
+//
+// Precondition: no buffer object bound to PIXEL_UNPACK_BUFFER
+func CompressedTexImage2DFromImage(target GLenum, level int, border int, img image.Image) {
+	bounds := img.Bounds()
+	if bounds.Empty() {
+		return
+	}
+
+	info, err := getCompressedImageInfo(img)
+	if err != nil {
+		return
+	}
+
+	ptr := unsafe.Pointer(reflect.ValueOf(info.Data).Index(0).UnsafeAddr())
+
+	C.glCompressedTexImage2D(C.GLenum(target), C.GLint(level), C.GLenum(info.Format),
+		C.GLsizei(bounds.Dx()), C.GLsizei(bounds.Dy()), C.GLint(border),
+		C.GLsizei(len(info.Data)), ptr)
 }
